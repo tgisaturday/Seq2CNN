b769abf95b537e9fe72b7c922e05cae3ea02f223
tgisaturday <jamesk1228@gmail.com>
2018-04-02 00:11:43 +0900

    parameter tuning

--- cnn_predict.py
+++ cnn_predict.py
@@ -20,22 +20,42 @@ def clean_str(s):
     """Clean sentence"""
     global counter_konlpy
     global total_dataset
+    global stopwords
     s = re.sub('[0-9]', '', s)
-    #kkma = Kkma()
-    # komoran  = Komoran()
-    # twitter = Twitter()
-    mecab = Mecab()
-    #print(' '.join(kkma.nouns(s)))
+    #s = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", s)
+    #s = re.sub(r"\'s", " \'s", s)
+    #s = re.sub(r"\'ve", " \'ve", s)
+    #s = re.sub(r"n\'t", " n\'t", s)
+    #s = re.sub(r"\'re", " \'re", s)
+    #s = re.sub(r"\'d", " \'d", s)
+    #s = re.sub(r"\'ll", " \'ll", s)
+    s = re.sub(r",", " , ", s)
+    s = re.sub(r"!", " ! ", s)
+    s = re.sub(r"\(", " \( ", s)
+    s = re.sub(r"\)", " \) ", s)
+    s = re.sub(r"\?", " \? ", s)
+    #s = re.sub(r"\s{2,}", " ", s)
+    #s = re.sub(r'\S*(x{2,}|X{2,})\S*',"xxx", s)
+    s = re.sub(r'[^\x00-\x7F]+', "", s)
+    s = s.strip()
+    #s = s.strip().lower()
+    max_length = 100
     result = []
-    result = mecab.nouns(s)
-    if len(result) > 1000:
-        result = result[0:1000]
-    elif len(result) < 1000:
-        result = result + ["<PAD/>"] * (1000 - len(result)-1)    
+    #result = s.split(' ')
+    mecab = Mecab()
+    result = mecab.nouns(s)   
+    
+    if len(result) >= max_length:
+        result = result[0:max_length]
+    elif len(result) < max_length:
+        result = result + ["<PAD/>"] * (max_length - len(result))
+        result = result[0:max_length]
+        
     counter_konlpy += 1
-    sys.stdout.write("\r Parsed: %d / %d" %(counter_konlpy, total_dataset))
+    sys.stdout.write("\rParsed: %d / %d" %(counter_konlpy, total_dataset))
     sys.stdout.flush()
-    return ' '.join(result)
+    
+    return ' '.join(result).strip()
 
 def predict_unseen_data():
     """Step 0: load trained model and parameters"""
@@ -47,17 +67,15 @@ def predict_unseen_data():
     logging.critical('Loaded the trained model: {}'.format(checkpoint_file))
 
     """Step 1: load data for prediction"""
-    test_file = "./dataset/description/"
-    columns = ['section', 'class', 'subclass', 'description']
-    selected = ['section', 'description']
+    test_file = "./dataset/abstract/"
+    columns = ['section', 'class', 'subclass', 'abstract']
+    selected = ['section', 'abstract']
     test_list = []
     for path, dirs, files in os.walk(test_file):
         if files:
             for filename in files:
                 fullname = os.path.join(path,filename)
-                if"2017" in filename.split('-')[0]:
-                    test_list.append(fullname)
-    random.shuffle(test_list)
+                test_list.append(fullname)
     #test_list = test_list[0:10000]
     data = []
     print("Listing all datas in testset.")
