47aa130d32d7339aeb4c555c402aa5a0e1b5f699
tgisaturday <jamesk1228@gmail.com>
2018-04-07 02:13:08 +0900

    using pre-embedded word embedding + bug fix

--- text_cnn.py
+++ text_cnn.py
@@ -55,7 +55,7 @@ class seq2CNN(object):
                     # Apply nonlinearity
                     h = tf.nn.relu(tf.nn.bias_add(conv, b), name='relu')
                     # Maxpooling over the outputs
-                    pooled = tf.nn.max_pool(h, ksize=[1, sequence_length - filter_size + 1, 1, 1], strides=[1, 1, 1, 1],
+                    pooled = tf.nn.max_pool(h, ksize=[1, max_summary_length - filter_size + 1, 1, 1], strides=[1, 1, 1, 1],
                                         padding='VALID', name='pool')
                     pooled_outputs.append(pooled)
             # Combine all the pooled features
